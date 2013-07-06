a627c6582b649d412f6cb6b681718f53f80c5330
Michael F <tehfreak@awesome39.com>
2013-07-06 14:14:30 +0400

    убрал фабрику сервиса в отдельный модуль

--- index.js
+++ index.js
@@ -1,53 +1,11 @@
-
 var Model= require('./lib/Model')
 var ModelService= require('./lib/ModelService')
-
-
-
-var $model= module.exports= $(ModelService, new ModelService('$model', {}, Model))
-
-
 
 /**
  * Сервис моделирования
  *
  * @name $model
  */
-function $(Service, service) {
-
-    if (!(Service instanceof Function)) {
-        throw Error('Cannot create service. Wrong class', Service)
-    }
-    if (!(service instanceof Service)) {
-        throw Error('Cannot create service. Wrong instance', service)
-    }
-
-    return Proxy.createFunction({
-        getOwnPropertyDescriptor: function(name) {
-            return Object.getOwnPropertyDescriptor(service)
-        },
-        getOwnPropertyNames: function() {
-            return Object.getOwnPropertyNames(service)
-        },
-        getPropertyDescriptor: function(name) {
-            return Object.getPropertyDescriptor(service)
-        },
-        getPropertyNames: function() {
-            return Object.getPropertyNames(service)
-        },
-        defineProperty: function(name, descriptor) {
-            return Object.defineProperty(service, name, descriptor)
-        },
-        get: function (proxy, k) {
-            return service[k]
-        }
-    }
-    ,   function (name, properties) {
-            return $(Service,
-                service.define(name, properties)
-            )
-    }
-    ,   function (data) {
-            return service.create(data)
-    })
-}
+var $model= module.exports= require('some-service')(
+    new ModelService('$model', {}, Model)
+)
--- readme.md
+++ readme.md
@@ -3,9 +3,13 @@
 Язык декларативного моделирования данных.
 
  
+## Требует гармонию
+Установите флаг запуска ```--harmony``` или ```--harmony_proxies```.
+
+ 
 ## Установка
 ```
 npm install https://github.com/somejs/some-model/archive/master.tar.gz
 npm test
 ```
-Зависит от **[some-schema](https://github.com/somejs/some-schema)**. Для тестирования необходимы **[mocha]()** и **[chai]()**.
+Зависит от **[some-schema](https://github.com/somejs/some-schema)** и **[some-service](https://github.com/somejs/some-service)**. Для тестирования необходимы **[mocha]()** и **[chai]()**.
